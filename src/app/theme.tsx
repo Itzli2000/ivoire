916c0df1bc3c93ed1e90b4d7e21a23242167ce97
Itzli Molina <itzli2000@msn.com>
2024-01-04 16:28:31 -0600

    feat: add MUI
    configure MUI custom theme
    configure standar and prettier linters

---
+++ src/app/theme.tsx
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#575F30",
+      main: "#373C1E",
+      dark: "#32361B",
+    },
+    secondary: {
+      light: "#B1C5AA",
+      main: "#9BB592",
+      dark: "#84A479",
+    },
+    warning: {
+      light: "#D6955C",
+      main: "#CC7B34",
+      dark: "#B46C2D",
+    },
+  },
+});
+
+export default theme;
